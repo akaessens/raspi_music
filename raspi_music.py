0e7e32e0a65fc337c0e245916fdf85129917043f
akaessens <24660231+akaessens@users.noreply.github.com>
2019-07-08 21:08:06 +0200

    added first spotify functionality with spotifyd/subprocess

--- raspi_music.py
+++ raspi_music.py
@@ -2,16 +2,28 @@ import logging
 import signal
 import sys
 import webradio
+import spotify
 
 
 class PiRadio(object):
 
     def __init__(self):
         self.webradio = None
+        self.spotify = None
         self.state = "off"
 
     def input(self, char):
-        if self.state == "webradio":
+        # OFF 
+        if self.state == "off":
+            if char == "w":
+                self.webradio = webradio.WebRadio()
+                self.state = "webradio"
+            elif char == "s":
+                self.spotify = spotify.Spotify()
+                self.state = "spotify"
+
+        # WEBRADIO
+        elif self.state == "webradio":
             if char == "x":
                 self.webradio.stop()
                 self.state = "off"
@@ -21,10 +33,12 @@ class PiRadio(object):
                 self.webradio.prev()
             elif char == " ":
                 self.webradio.play_pause()
-        elif self.state == "off":
-            if char == "w":
-                self.webradio = webradio.WebRadio()
-                self.state = "webradio"
+ 
+        # SPOTIFY
+        elif self.state == "spotify":
+            if char == "x":
+                self.spotify.stop()
+                self.state = "off"
 
 def sigint_handler(signum, frame):
     radio.input("x")
@@ -32,13 +46,12 @@ def sigint_handler(signum, frame):
 
 
 if __name__ == '__main__':
-
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.DEBUG,format= '%(asctime)s [%(levelname)s] %(message)s', datefmt='%H:%M:%S')
     radio = PiRadio()
     signal.signal(signal.SIGINT, sigint_handler)
     signal.signal(signal.SIGTERM, sigint_handler)
 
-    print ("Controls: \nSpace\tplay/pause\n<\tPrevious\n>\tNext\nx\tStop\nw\tWebradio\nCtrl+C\tExit\n")
+    print ("Controls: \nSpace\tplay/pause\n<\tPrevious\n>\tNext\nx\tStop\nw\tWebradio\ns\tSpotify\nCtrl+C\tExit\n")
     
     while (True):
         char = sys.stdin.read(1)
---
+++ spotify.py
@@ -0,0 +1,14 @@
+from webradio import Player
+import logging
+import subprocess
+
+
+class Spotify(Player):
+
+    def __init__(self):
+        logging.debug("spotifyd started")
+        self.process = subprocess.Popen("~/workspace/spotifyd/target/release/spotifyd --no-daemon", shell=True)
+
+    def stop(self):
+        self.process.kill()
+        logging.debug("stopped spotifyd")
